8bb711a909a83fdd1c911ce96b798a269ff09fb3
MdEzazullah <151666172+MdEzazullah@users.noreply.github.com>
2026-01-04 18:59:38 +0000

    string

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,29 +1,18 @@
-// //Strings can be represented as "xyz" 'xyz' '''xyz'''
-// const name = "Md Ezazullah"
-// const repoCount = 50 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
-// const myGame = new String('Ezazullah')
-// console.log(myGame[0]);
-// // console.log(myGame__proto__);
-// console.log(myGame.length)
-// console.log(myGame.toUpperCase);
-// console.log(myGame.toLocaleLowerCase);
-// console.log(myGame.)
 
 
-//trim method
-let msg = "  Hello  ";
-console.log(msg.trim());
-let password = "          kajdj ojf da jaijana jja ijjakpsj jj j           "
-console.log(password)
-console.log(password.trim());
 //string are immutable in JS
-console.log(msg);
+let myDept = "CSE";
+console.log(myDept);//CSE
+myDept = "ECE";
+console.log(myDept)//ECE
+let str1 = "Palak";
+str1[0]="F";
+console.log(str1);//Palak
 
 //toUpperCase() and toLOwerCase() method
-// let name = "Md Ezazullah";
-// console.log(name.toUpperCase());
-// console.log(name.toLowerCase());
+let myName = "Md Ezazullah";
+console.log(myName.toUpperCase());//MD EZAZULLAH
+console.log(myName.toLowerCase());//md ezazullah
 
 //string method with argument 
 //1.indexOf method
@@ -37,13 +26,20 @@ console.log(str.indexOf("C"));
 console.log(str.indexOf("love"));//output -> -1
 
 //method chaining
+let msg = "  Hello  ";
 let newMsg= msg.trim();
 console.log("Before trim :",msg);
 console.log("After trim :",newMsg);
+
 newMsg=newMsg.toUpperCase();
 console.log("After Uppercase :",newMsg);
 //in single line 
 console.log(msg.trim().toUpperCase());  
+
+let password = "          kajdj ojf da jaijanatyutya iqwerakpsj jj j           "
+console.log(password)//          kajdj ojf da jaijanatyutya iqwerakpsj jj j           
+console.log(password.trim());//kajdj ojf da jaijanatyutya iqwerakpsj jj j
+console.log(password.toUpperCase());
 
 //slice method
 let STR="ILoveCoding";
@@ -54,12 +50,17 @@ console.log(STR.slice(-2));    //'ILoveCodi'
 
 //replace method
 let S = "ApnaCollege";
-console.log(S.replace("A","S"));
-console.log(S.replace("Apna","Tera"));
+console.log(S.replace("A","S"));//SpnaCollege
+console.log(S.replace("Apna","Tera"));//TeraCollege
+const url = "https://Ezaz.com/Ezaz%20Ansari";
+console.log(url.replace("%20", "-"))
+console.log(url.includes("Hitesh"))//false
+console.log(url.includes("Ezaz"))
+
 
 //repeat method
 let S1="Mango";
-console.log(S1.repeat(3));
+console.log(S1.repeat(3));//MangoMangoMango
 
 //Practice Question
 let ques="Help!";//Trim it and convert it to uppercase
@@ -72,3 +73,8 @@ console.log(name.indexOf("na"));         // 2
 console.log(name.replace("Apna","Our")); //Our College
 console.log(name.slice(4).replace("l","t").replace("l","t"));
 //string print in the output is the new string because string is immutable(not changeble)!!!
+
+const Name = "shah rukh khan";
+const repoCount = 50;
+console.log(`Koyla actor is ${Name} and the repo value is ${repoCount}`);
+
